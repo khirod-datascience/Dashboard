724ec5b24fb4d32bde923c22fff812e8991657c2
khirod-datascience <khirod.datascience@gmail.com>
2023-06-09 08:28:12 +0530

    push

---
+++ AdminDashboard/student/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    
+    path('form/',views.Applicant_details,name='Applicant_details'),
+    path('seminar/',views.Seminar_report,name='seminar_report'),
+    path('project/',views.Projects_report,name='projects_report'),
+    path('assign/',views.Assignment,name='assignment'),
+]
---
+++ AdminDashboard/student/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Studentform(request):
+    return render(request,'studentform.html')
+
+def Applicant_details(request):
+    return render(request,'applicant_details.html')
+
+def Seminar_report(request):
+    return render(request,'seminar_report.html')
+
+def Projects_report(request):
+    return render(request,'project_report.html')
+
+def Assignment(request):
+    return render(request,'Assignment.html')
